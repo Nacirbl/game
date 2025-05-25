35812a62e86470b53e98bd0fe0652c75788397c9
Nacirbl <33876543+Nacirbl@users.noreply.github.com>
2025-05-25 17:23:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -659,4 +659,5 @@ def get_player_mapping(session_data, friend_data=None):
         }
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 locally
+    app.run(host='0.0.0.0', port=port)
